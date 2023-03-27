7dcb32b96e6e4dac74883e96629860856fa9a5ce
Reuben-Wilson123 <reubenwilson049@gmail.com>
2023-03-27 16:57:46 +0100

    leaderboard works

--- scoreboard.py
+++ scoreboard.py
@@ -1,26 +1,38 @@
 import tkinter as tk 
 
 def leaderboard():
-    counter = 0
-    #lb = tk.Tk()
-    #lb.title("Leaderboard")
-    #lb.geometry("1000x500")
+    counter = 1
+    lb = tk.Tk()
+    lb.title("Leaderboard")
+    lb.geometry("1000x500")
     file = open("leaderboard.txt","r")
     scorelist = []
-    for line in file:
-        scorelist.append(line)
-    file.close()
-    for item in scorelist:
-        try:
-            item1, item2 = item.split(", ")
-            item2 = item2.strip()
-
-            scorelist[counter] = ((counter+1),"Name = ", item1, "Score = ",item2)
-        except:
-            pass
-        counter += 1 
-    print(scorelist)
-    #lbl1 = tk.Label(lb,text = sb)
-    #lb.mainloop()
+    board = ""
+    items = ""
+    output = ""
+    try:
+        with open("leaderboard.txt") as f:
+            for line in f:
+                name, score = line.split(', ')
+                score = int(score)
+                scorelist.append((name, score))
+            scorelist.sort(key=lambda s: s[1])
+            for item in scorelist:
+                board,items = "",""
+                for i in range(len(item)):
+                    items += str(item[i])
+                    if i == 0:
+                        items += " = "
+                board = str(counter)
+                board +=" - "
+                board += items
+                board+="\n"
+                counter+=1
+                output +=board
+    except:
+        pass
+    lbl1 = tk.Label(lb,text = output, font = ("Arial",20),justify = "left")
+    lbl1.place(x=100,y=100)
+    lb.mainloop()
 
 leaderboard()
